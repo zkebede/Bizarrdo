53ccff36af9c9cbfb3cf6a02f630396cdefe861b
zkebede <zemenk6@gmail.com>
2022-09-29 14:49:06 -0400

    Create products.php

---
+++ products.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bizarrdo's Chips</title>
+    <!-- font family  google-->
+    <link href="https://fonts.googleapis.com/css2?family=Recursive&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.php">
+</head>
+
+<body>
+    <!-- header and nav bar html -->
+    <div class="header_product">
+        <div class="container">
+            <!-- nav Bar html -->
+            <div class="navbar">
+                <div class="log">
+                    <img src="/img/Bizarrdos_Chips_V2 (1).png" width="125px" alt="logo Website">
+                </div>
+                <nav>
+                    <ul id="MenuItems">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="products.html">Products</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="cart.html">Cart</a></li>
+                        <li><a href="account.html">Account</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-------------featured products -->
+    <div class="products_page">
+        <div class="small-container">
+            <div class="row">
+                <h2 class="title">All Products</h2><br>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img src="/img/Sour Cream and Onion White Background.jpg" alt="product-1">
+                    <h4>Sour Cream and Onion</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half-o"></i>
+                    </div>
+                    <p>$2.99</p>
+                </div>
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag Classic White Background.jpg" alt="product-2">
+                    <h4>Classic</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$2.99.</p>
+                </div>
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag Spicy White Background.jpg" alt="product-3">
+                    <h4>Flamin Hot</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$2.99</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag Salt and Vin White Background.jpg" alt="product-11.jpg">
+                    <h4>Salt and Vin</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$2.99</p>
+                </div>
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag Limon White Background.jpg" alt="product-10">
+                    <h4>Limon</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$3.99</p>
+                </div>
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag BBQ White Background.jpg" alt="product-9">
+                    <h4>Barbecue</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half-o"></i>
+                    </div>
+                    <p>$2.99</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img src="/img/Bizarrdo Bag Cheesy White Background.jpg" alt="product-11.jpg">
+                    <h4>Cheesy Flavored</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$2.99</p>
+                </div>
+            </div>
+
+            <div class="page-btn">
+                <span>1</span>
+                <span>2</span>
+            </div>
+        </div>
+    </div>
+
+    <!------------------- footer--------------- -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-3">
+                    <h3>UseFul Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                    </ul>
+                </div>
+                <h1>Bizarrdo's Chips</h1>
+
+                <div class="footer-col-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">BMGT 407 - 2022</p>
+        </div>
+    </div>
+    <!-- script -->
+    <script src="script.js"></script>
+</body>
+
+</html>
