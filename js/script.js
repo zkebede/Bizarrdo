50e78020909d2d8c6eca3fefa5ead9b978fa51d0
zkebede <zemenk6@gmail.com>
2022-09-29 18:01:42 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,18 +3,6 @@ let MenuItems = document.getElementById("MenuItems");
 let chsecer = MenuItems.style.maxHeight = "0px";
 let menu = document.getElementById("menu");
 
-function menutoggle() {
-    if(chsecer == "0px"){
-        chsecer = MenuItems.style.maxHeight = "55vh";
-        menu.src = "images/close.svg";
-    }
-    else{
-        chsecer = MenuItems.style.maxHeight = "0px";
-        menu.src = "images/menu.png";
-
-    }
-}
-
 // -------script for one product page gallery-----
 let ProductImg = document.getElementById('ProductImg');
 let SmallImg = document.getElementsByClassName('SmallImg');
